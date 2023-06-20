719fca94a3d3e724bc28554b7700d3d73d8a97a7
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-06-20 15:12:39 +1000

    resolved issue of new merges overwriting old and removed my directory path

--- pdf_merger.py
+++ pdf_merger.py
@@ -2,12 +2,22 @@ import PyPDF2
 import sys
 import os
 
-os.chdir('/Users/vietle/code/pdf-merger/pdf-files-to-merge')
+# Change the directory to where your copy of the repository is saved
+sourceDirectoryToMerge = ''
+os.chdir(sourceDirectoryToMerge)
 merger = PyPDF2.PdfMerger(strict=False)
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        # print(file)
         merger.append(file)
-os.chdir('/Users/vietle/code/pdf-merger/merged-pdf')
-merger.write("Combined_PDF.pdf")
+
+# Change the directory to where your copy of the repository is saved
+destinationDirectory = ''
+os.chdir(destinationDirectory)
+
+listOfPDFs = []
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        listOfPDFs.append(file)
+
+merger.write("CombinedPDF_" + str(len(listOfPDFs)) +".pdf")
