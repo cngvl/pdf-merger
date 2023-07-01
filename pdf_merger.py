303f43e20e350d6a890d0717641a9e26ec52a68b
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2023-07-01 15:17:07 +1000

    solved issue with PDFs were being merged in the incorrect order

--- pdf_merger.py
+++ pdf_merger.py
@@ -7,17 +7,24 @@ sourceDirectoryToMerge = ''
 os.chdir(sourceDirectoryToMerge)
 merger = PyPDF2.PdfMerger(strict=False)
 
-for file in os.listdir(os.curdir):
-    if file.endswith(".pdf"):
-        merger.append(file)
+fileNumber = 1
+numberOfFiles = len(os.listdir(os.curdir)) - 2
+
+while fileNumber <= numberOfFiles:
+    for file in os.listdir(os.curdir):
+        if file.endswith(f'{fileNumber}.pdf'):
+            merger.append(file)
+            fileNumber += 1
 
 # Change the directory to where your copy of the repository is saved
 destinationDirectory = ''
 os.chdir(destinationDirectory)
 
-listOfPDFs = []
+# For counting the number of PDF files and then naming the latest merged PDF accordingly
+numberOfMergedPDFs = 0
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        listOfPDFs.append(file)
+        # listOfPDFs.append(file)
+        numberOfMergedPDFs += 1
 
-merger.write("CombinedPDF_" + str(len(listOfPDFs)) +".pdf")
+merger.write("CombinedPDF_" + str(numberOfMergedPDFs) +".pdf")
